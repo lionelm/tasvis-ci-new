0da6c837d6850fc30eaaf48308f73dc572155631
dinhhv@tasvis.com.vn <dinhhv@tasvis.com.vn@cb0011e7-1b81-dbc3-1490-3ff623f8c5f4>
2012-10-22 18:08:05 +0000

    Thêm views/user_add.php

---
+++ trunk/source/codeigniter/application/modules/administrator/views/user_add.php
@@ -0,0 +1,33 @@
+    
+<ul class="maintabmenu multipletabmenu">  
+    <li ><a href="<?php echo base_url();?>administrator/users">Tất cả người dùng</a></li>
+    <li class="current"><a href="<?php echo base_url();?>administrator/users/add">Thêm mới người dùng</a></li> 
+</ul> 
+     
+<div class="content">                	
+        <div class="edit-left">
+            <form action="<?php echo base_url();?>administrator/users/add" method="post" accept-charset="utf-8" id="formID" class="stdform">                    		
+            <p><label>Họ tên:</label></p>
+            <p><span class="field"><input name="txtnicename" value="" id="txtnicename" class="longinput validate[required]" type="text"></span></p>
+            <br>
+            
+            <p><label>Tên truy cập:</label></p>
+            <p><span class="field"><input class="longinput validate[required]" name="txtlogin" type="text"></span></p>
+            <br> 
+            
+            <p><label>Mật khẩu:</label></p>
+            <p><span class="field"><input class="longinput validate[required]" name="txtpass" type="password"></span></p>
+            <br>
+            
+            <p><label>Email:</label></p>
+            <p><span class="field"><input class="longinput validate[required]" name="txtemail" type="text"></span></p>
+            <br>
+            
+                       
+            <p class="stdformbutton">
+                <input name="submit" value="Add" class="submit radius2" type="submit">                                
+                <input value="Cancel" class="reset radius2" type="reset">
+            </p>                            
+        </form>                                        
+        </div>
+ </div>       
