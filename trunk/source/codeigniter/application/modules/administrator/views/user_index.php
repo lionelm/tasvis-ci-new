58e6760a14ca6a023ac6a57617748bdf727f5386
dinhhv@tasvis.com.vn <dinhhv@tasvis.com.vn@cb0011e7-1b81-dbc3-1490-3ff623f8c5f4>
2012-10-22 18:09:16 +0000

    Thêm views/user_index.php

---
+++ trunk/source/codeigniter/application/modules/administrator/views/user_index.php
@@ -0,0 +1,66 @@
+
+<ul class="maintabmenu multipletabmenu">     
+    <li class="current"><a href="<?php echo base_url();?>administrator/users">Tất cả người dùng</a></li>
+    <li><a href="<?php echo base_url();?>administrator/users/add">Thêm mới người dùng</a></li>    
+</ul>    
+
+<div class="content">                	
+       <div class="contenttitle radiusbottom0">        
+            <h2 class="table"><span>Danh sách người dùng</span></h2>
+        </div><!--contenttitle-->
+        <div class="tableoptions">
+            <button class="deletebutton radius3" name="delete_term" value="<?php echo base_url();?>administrator/users/delete" title="table2">Delete Selected</button> &nbsp;                           
+        </div><!--tableoptions-->	
+        <table id="table2" class="stdtable stdtablecb" border="0" cellpadding="0" cellspacing="0">
+            <colgroup>
+                <col class="con0">
+                <col class="con1">
+                <col class="con0">
+                <col class="con1">
+
+            </colgroup>
+            <thead>
+                <tr>
+                    <th class="head0" width="10"><input class="checkall" type="checkbox"></th>
+                    <th class="head1">Họ tên</th>
+                    <th class="head0">Ten truy cập</th>
+                    <th class="head0">Mật khẩu</th>
+                    <th class="head0">Email</th>
+                    <th class="head0" width="60">&nbsp;</th>
+                </tr>
+            </thead>
+            <tfoot>
+                <tr>
+                    <th class="head0" width="10"><input class="checkall" type="checkbox"></th>
+                    <th class="head1">Họ tên</th>
+                    <th class="head0">Tên truy cập</th>
+                    <th class="head0">Mật khẩu</th>
+                    <th class="head0">Email</th>
+                    <th class="head0" width="60">&nbsp;</th>
+                </tr>
+            </tfoot>
+            <tbody>
+                <?php 
+                    foreach ($lstuser as $user)
+                    {
+                ?>
+                <tr>
+                    <td class="center"><input value="<?php echo $user->id;?>" type="checkbox"></td>
+                    <td>
+                    <?php 
+                        echo $user->user_nicename;
+                    ?>
+                    </td>
+                    <td><?php echo $user->user_login;?></td>
+                    <td><?php echo $user->user_pass;?></td>
+                    <td><?php echo $user->user_email;?></td>                    
+                    <td class="center"><a class="edit" title="Sửa" href="<?php echo base_url();?>administrator/users/edit/<?php echo $user->id;?>">Sửa</a> &nbsp; <a class="delete" id="<?php echo $user->id;?>" name="delete" title="Xóa danh mục" href="<?php echo base_url();?>administrator/users/delete">Xóa</a></td>
+                </tr>  
+                <?php
+                    }
+                ?>
+            </tbody>
+           
+        </table>         
+                                  
+</div><!--content-->                
